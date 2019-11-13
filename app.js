309cae8509bf46bc14a5abca25e1d396e4719521
root <nlcopping@gmail.com>
2019-11-13 12:57:13 +0000

    Added routes for login, about, signup and new review, added basic form for sign up

--- app.js
+++ app.js
@@ -4,15 +4,29 @@ const express    = require("express"),
 	
 app.set("view engine", "ejs");				  
 		  
+// Route for main page
 app.get("/", (req, res) => {
 	res.render("landing");
 });					  
-					  
-	app.get("/signup", (req, res) => {
+// Route for sign up page			  
+app.get("/signup", (req, res) => {
 		res.render("signup");
-	});				  
-					  
-					  
+});				  
+// Route for login page
+app.get("/login", (req, res) => {
+		res.render("login");
+});	
+// Route for about page
+app.get("/about", (req, res) => {
+		res.render("about");
+});
+// Route for new review page
+app.get("/newreview", (req, res) => {
+		res.render("newreview");
+});
+
+
+// Server listening 					  
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log("Code Review is up and running!");
