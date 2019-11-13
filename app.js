c00e25b75af1a092dc0522229f07259137aff7b5
root <nlcopping@gmail.com>
2019-11-13 19:10:40 +0000

    added array with some seed data and logic to pass htat through the the review page. Added some light html & ejs to display that data in landing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express    = require("express"),
 	  app        = express(),
 	  bodyParser = require("body-parser"),
-	  mongoose   = reguire("mongoose");
+	  mongoose   = require("mongoose");
 	
 app.set("view engine", "ejs");				  
 		  
@@ -11,9 +11,8 @@ app.get("/", (req, res) => {
 		{title: "Noah's Normal Node Nook", author:"Noah", review:"Course about Node", image: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"},
 		{title: "Shivan's Super Scala Story", author:"Shivan", review:"Scala information", image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80"},
 		{title: "Dario's Deep Database Dive", author:"Dario", review:"Databases in Depth", image: "https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"}
-		
-	]
-	res.render("landing");
+		]
+	res.render("landing",{reviews:reviews});
 });					  
 // Route for sign up page			  
 app.get("/signup", (req, res) => {
