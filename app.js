2bed201f11b458830cd76362b53c1dc0e4657272
root <nlcopping@gmail.com>
2019-11-27 13:51:06 +0000

    Added </> for html and body to footer. included fotter on files that didnt have it. Removed redundant closing tags.

--- app.js
+++ app.js
@@ -28,16 +28,6 @@ const reviewSchema = new mongoose.Schema({
 
 const Review = mongoose.model("Review", reviewSchema);
 
-// Code to Manually add a Review to Database
-// Review.create(
-// 	{title: "Dario's Deep Database Dive", author:"Dario", review:"Databases in Depth", image: "https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"}, (err, review) => {
-// 		if(err) {
-// 			console.log("Error");
-// 		} else {
-// 			console.log("New Review Created");
-// 			console.log(review);
-// 		}
-// 	});
 
 app.get("/", (req, res) => {
 	res.redirect("/courses");
@@ -92,17 +82,6 @@ app.post("/search", (req, res) => {
 });
 		
 
-
-
-	
-	// const reviews = [
-	// 	{title: "Noah's Normal Node Nook", author:"Noah", review:"Course about Node", image: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"},
-	// 	{title: "Shivan's Super Scala Story", author:"Shivan", review:"Scala information", image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80"},
-	// 	{title: "Dario's Deep Database Dive", author:"Dario", review:"Databases in Depth", image: "https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"}
-	// 	]
-	// 
-
-
 // Create - Route to handle info from form and add a new course to DB
 app.post("/courses", (req, res) => {
 // 	Get fields from form and save in newReview variable
@@ -131,9 +110,6 @@ app.post("/courses", (req, res) => {
 });
 
 
-
-
-
 // Route for sign up page			  
 app.get("/signup", (req, res) => {
 		res.render("signup");
@@ -150,7 +126,6 @@ app.get("/about", (req, res) => {
 app.get("/error", (req, res) => {
 		res.render("error");
 });
-
 
 
 // Server listening 					  
